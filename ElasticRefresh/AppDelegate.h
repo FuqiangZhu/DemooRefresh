604eb63d0ea2c8020eb387fcea464617a6b4cf1c
强哥 <qiang@qq.com>
2016-07-13 15:44:29 +0800

    Initial Commit

---
+++ ElasticRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ElasticRefresh
+//
+//  Created by qiang on 16/7/13.
+//  Copyright © 2016年 Qiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ElasticRefresh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ElasticRefresh
+//
+//  Created by qiang on 16/7/13.
+//  Copyright © 2016年 Qiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
